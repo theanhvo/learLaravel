307b13457819e3b22db5e2415bca881ce5749dde
The Anh <admin@The-Anh-Vo.local>
2020-11-20 10:16:00 +0700

    first commit

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyController extends Controller
+{
+    public function XinChao()
+    {
+        echo "Chao Cac ban tre";
+    }
+
+    public function KhoaHoc($ten)
+    {
+        echo "Khoa hoc: $ten";
+        return redirect()->route('route2');
+    }
+
+    public function GetUrl(Request $request)
+    {
+        if ($request->is('My*')) { // is la check partern
+            echo "la mycontrol";
+        } else {
+            echo "not mycontrol";
+        }
+        
+        // if ($request->isMethod('get')) {
+        //     echo "method get";
+        // } else {
+        //     echo "not method get";
+        // }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/khoahoc', function () {
+    echo "test khoa hoc";
+});
+
+Route::get('/khoahoc/{id}', function ($id) {
+    echo "<h1> test $id";
+})->where(['id' => '[0-9]+']);
+
+
+Route::get('page', function () {
+    echo "ve page ne ma";
+})->name('route2'); // name định danh
+
+Route::get('pagecon', function () {
+    return redirect()->route('route2'); // redirect về biến định danh
+});
+
+Route::group(['prefix' => 'MyGroup'], function () {
+    Route::get('user1', function () {
+        echo "user1";
+    });
+});
+
+Route::get('GetMyController', 'MyController@XinChao');
+Route::get('tham-so/{ten}', 'MyController@KhoaHoc');
+
+Route::get('my-request', 'MyController@GetUrl');
